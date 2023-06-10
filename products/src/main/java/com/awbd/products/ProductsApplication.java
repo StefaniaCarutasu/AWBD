18fb6edc373839d4e9ffcdb7acd1ed7d9f63bd93
Madalina Nicolescu <madalina_maria04@yahoo.com>
2023-06-10 23:48:01 +0300

    eureka

--- products/src/main/java/com/awbd/products/ProductsApplication.java
+++ products/src/main/java/com/awbd/products/ProductsApplication.java
@@ -2,8 +2,14 @@ package com.awbd.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ProductsApplication {
 
     public static void main(String[] args) {
